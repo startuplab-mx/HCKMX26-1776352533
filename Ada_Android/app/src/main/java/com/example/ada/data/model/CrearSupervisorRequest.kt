c503ad6fda86ec9e06c861501040014828a7453b
Cano's Code  (；●∀●)ゝ” <99841453+AlejandroTRIKITRAKATELAS33sanchezCano@users.noreply.github.com>
2026-04-25 14:36:57 -0600

    Primer Response y Request de supervisor

---
+++ Ada_Android/app/src/main/java/com/example/ada/data/model/CrearSupervisorRequest.kt
@@ -0,0 +1,5 @@
+package com.example.ada.data.model
+
+data class CrearSupervisorRequest(
+
+)
--- Ada_Android/app/src/main/java/com/example/ada/data/remote/ApiService.kt
+++ Ada_Android/app/src/main/java/com/example/ada/data/remote/ApiService.kt
@@ -2,6 +2,8 @@ package com.example.ada.data.remote
 
 import com.example.ada.data.model.CrearSupervisadoRequest
 import com.example.ada.data.model.CrearSupervisadoResponse
+import com.example.ada.data.model.CrearSupervisorRequest
+import com.example.ada.data.model.CrearSupervisorResponse
 import com.example.ada.data.model.LoginRequest
 import com.example.ada.data.model.LoginResponse
 import retrofit2.Response
@@ -17,6 +19,10 @@ interface ApiService {
         @Body request: CrearSupervisadoRequest
     ): Response<CrearSupervisadoResponse>
 
+    @POST()
+    suspend fun crearSupervisor(
+        @Body request: CrearSupervisorRequest
+    ): Response<CrearSupervisorResponse>
 
     @POST("api/login")
     suspend fun login(
