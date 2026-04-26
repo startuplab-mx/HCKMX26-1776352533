77b03030f4dd93bc952b5e1296ab9fbe42451454
Rafsa07 <rafaelguesa5@gmail.com>
2026-04-25 18:53:33 -0600

    Delete ADA1 directory

--- ADA1/app/src/main/java/com/example/ada_1/ui/screens/ChatNinoUI.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ada.ui.screens
-
-data class ChatNinoUI(
-    val id: String,
-    val nombreCompleto: String,
-    val ultimoMensaje: String,
-    val hora: String,
-    val appIcon: Int? = null
-)
--- ADA1/app/src/main/java/com/example/ada_1/ui/theme/Theme.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ada.ui.theme
-import androidx.compose.ui.graphics.Color
-
-val DarkMaroon = Color(0xFF1E020B)
-val LightMaroon = Color(0xFF4C041B)
